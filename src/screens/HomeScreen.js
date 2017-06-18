0d6bd07ba58eb6d43a81c8d3548fbc6ebb99d254
David Henley <henleydr@gmail.com>
2017-06-18 14:13:02 -0500

    Getting user location with Expo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text } from 'native-base';
-import { MapView } from 'expo';
+import { View } from 'native-base';
+import { MapView, Location, Permissions } from 'expo';
 import { connect } from 'react-redux';
 
 class HomeScreen extends Component {
   state = {
     region: {
-      latitude: 37.78825,
-      longitude: -122.4324,
+      latitude: 36.174465,
+      longitude: -86.767960,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }
+  }
+
+  _getLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (!status === 'granted') {
+      console.error('You must grant location permissions.');
+    } else {
+      let position = await Location.getCurrentPositionAsync({
+        enableHighAccuracy: true
+      });
+      this.setState({
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }
+      });
+    }
+  }
+
+  _onRegionChange = (region) => {
+    // console.log('Region changed', region);
+  }
+
+  _onRegionChangeComplete = (region) => {
+    // console.log('Region change completed', region);
+  }
+
+  async componentDidMount() {
+    this._getLocation();
   }
 
   render() {
     const { region } = this.state;
 
     return (
-      <View style={styles.container}>
+      <View style={{ flex: 1 }}>
         <MapView
           provider={MapView.PROVIDER_GOOGLE}
           style={{ flex: 1 }}
           region={region}
+          onRegionChange={this._onRegionChange}
+          onRegionChangeComplete={this._onRegionChangeComplete}
         >
           <MapView.Marker
             coordinate={region}
@@ -32,12 +65,6 @@ class HomeScreen extends Component {
     );
   }
 }
-
-const styles = {
-  container: {
-    flex: 1
-  }
-};
 
 const mapStateToProps = (state) => {
   return state;
